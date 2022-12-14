3e1d7676b288567343bd63b1cf7f434fdf85418d
Sahid Rahman <sahidrahman404@gmail.com>
2022-12-14 23:05:29 +0700

    add function to handle expense form

--- app/routes/__app/expenses/add.tsx
+++ app/routes/__app/expenses/add.tsx
@@ -1,8 +1,11 @@
 // /expenses/add
 
+import { connect } from "@planetscale/database/dist";
+import { redirect } from "@remix-run/cloudflare";
 import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
+import { config, Env } from "~/db/dbConfig.server";
 
 export default function AddExpensesPage() {
   const navigate = useNavigate();
@@ -16,4 +19,22 @@ export default function AddExpensesPage() {
       <ExpenseForm />
     </Modal>
   );
+}
+
+export async function action({
+  request,
+  context,
+}: {
+  request: Request;
+  context: Env;
+}) {
+  const formData = await request.formData();
+  const conn = connect(config(context));
+  const title = formData.get("title");
+  const amount = formData.get("amount");
+  const date = formData.get("date");
+  const query = "INSERT INTO expense (title, amount, date) VALUES(?,?,?)";
+  const params = [title, Number(amount), date];
+  const result = await conn.execute(query, params);
+  return redirect("/expenses");
 }
