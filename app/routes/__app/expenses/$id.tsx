0bcd019286babef6f74747d59715db33d738e88c
Sahid Rahman <sahidrahman404@gmail.com>
2022-12-17 14:31:02 +0700

    implement updateExpense function in updateExpense page

--- app/routes/__app/expenses/$id.tsx
+++ app/routes/__app/expenses/$id.tsx
@@ -1,5 +1,10 @@
+import { json, redirect } from "@remix-run/cloudflare";
+import { Params, useActionData } from "@remix-run/react";
+import { inputFromForm } from "domain-functions";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
+import { Env } from "~/db/dbConfig.server";
+import { updateExpense } from "~/db/expense.server";
 // import type { Env } from "~/db/dbConfig.server";
 // import { getExpense } from "~/db/expense.server";
 // import { loaderResponseOrThrow } from "~/lib/index";
@@ -10,6 +15,23 @@ export default function UpdateExpensesPage() {
       <ExpenseForm />
     </Modal>
   );
+}
+
+export async function action({
+  params,
+  request,
+  context,
+}: {
+  params: Params;
+  request: Request;
+  context: Env;
+}) {
+  const input = await inputFromForm(request);
+  const result = await updateExpense({ ...params, ...input }, context);
+  if (!result.success) {
+    return json(result);
+  }
+  return redirect("/expenses");
 }
 
 // export type LoaderData = typeof loader;
