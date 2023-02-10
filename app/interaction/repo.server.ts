1ac4fd528512cfa47ca946993dbf50c6c5738ac0
Sahid Rahman <sahidrahman404@gmail.com>
2023-02-10 22:32:03 +0700

    add account type as a Database type property

--- app/interaction/repo.server.ts
+++ app/interaction/repo.server.ts
@@ -1,18 +1,25 @@
-import { CamelCasePlugin } from "kysely";
-import { Kysely } from "kysely";
+import { CamelCasePlugin, Kysely } from "kysely";
 import { PlanetScaleDialect } from "kysely-planetscale";
+import type { Account } from "~/domain/data/account/accountSchema.server";
 import type { Env } from "~/domain/data/env.server";
 import type { Expense } from "~/domain/data/expenses/expenseSchema.server";
 
 export type Database = {
   expenses: Expense;
+  accounts: Account;
 };
 
-export const repo = ({
+type Repo = ({
   DATABASE_HOST,
   DATABASE_PASSWORD,
   DATABASE_USERNAME,
-}: Env) => {
+}: Env) => Kysely<Database>;
+
+export const repo: Repo = ({
+  DATABASE_HOST,
+  DATABASE_PASSWORD,
+  DATABASE_USERNAME,
+}) => {
   return new Kysely<Database>({
     dialect: new PlanetScaleDialect({
       host: DATABASE_HOST,
