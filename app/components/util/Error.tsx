2ee42edf29fff07e3708e322cabea58137f1ba9d
Sahid Rahman <sahidrahman404@gmail.com>
2023-02-13 17:20:25 +0700

    fix type name

--- app/components/util/Error.tsx
+++ app/components/util/Error.tsx
@@ -1,21 +1,22 @@
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 import { FaExclamationCircle } from "react-icons/fa";
 
-type Error = {
-    title: string;
-    children: ReactNode;
+type ErrorInput = {
+  title: string;
+  children: ReactNode;
 };
 
-function Error({ title, children }: Error) {
-    return (
-        <div className="error">
-            <div className="icon">
-                <FaExclamationCircle />
-            </div>
-            <h2>{title}</h2>
-            {children}
-        </div>
-    );
+function Error({ title, children }: ErrorInput) {
+  return (
+    <div className="error">
+      <div className="icon">
+        <FaExclamationCircle />
+      </div>
+      <h2>{title}</h2>
+      {children}
+    </div>
+  );
 }
 
 export default Error;
+export type { ErrorInput };
